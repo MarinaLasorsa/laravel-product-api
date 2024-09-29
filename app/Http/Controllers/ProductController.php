c28d5c5294c8f8daf38eb4137310f070b4066180
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-09-29 11:33:43 +0200

    ADD: crud functions in product controller + resource routes in api file

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,92 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return response()->json($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+
+        if ($request->has('category_ids')) {
+            $product->categories()->attach($request->category_ids);
+        }
+
+        return response()->json($product);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        $product = Product::find($id);
+
+        if($product) {
+
+            return response()->json($product);
+
+        } else {
+
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, int $id)
+    {
+        $product = Product::find($id);
+
+        if($product) {
+
+            $product->update($request->all());
+
+            if ($request->has('category_ids')) {
+                $product->categories()->sync($request->category_ids);
+            } else {
+                $product->categories()->detach();
+            }
+
+            return response()->json($product);
+
+        } else {
+
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(int $id)
+    {
+        $product = Product::find($id);
+
+        if($product) {
+
+            $product->delete();
+
+            return response()->json(['message' => 'Product deleted']);
+
+        } else {
+
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    }
 }
